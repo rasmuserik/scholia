730c5413b01da96dc0c133adb4e01c4eeeca1f97
RasmusErik Voel Jensen <gitlab@solsort.com>
2021-04-23 19:28:46 +0200

    only tested visualisations as inline + add compare-page + add link to github-sparql + better load-message

--- scholia/app/static/scholiaSparqlVisualisations.js
+++ scholia/app/static/scholiaSparqlVisualisations.js
@@ -7,37 +7,41 @@
   //
   let visualisations = {
     BarChart: { type: "inline" },
-    BubbleChart: { type: "inline" },
-    Dimensions: { type: "inline", resultBrowser: "MultiDimension" },
+    BubbleChart: { type: "iframe" },
+    Dimensions: { type: "iframe", resultBrowser: "MultiDimension" },
     Graph: { type: "inline" },
-    ImageGrid: { type: "iframe", resultBrowser: "Image" }, // TODO styling
-    LineChart: { type: "inline" },
-    Map: { type: "inline", resultBrowser: "Coordinate" },
-    ScatterChart: { type: "inline" },
-    Table: { type: "inline" },
-    Timeline: { type: "iframe" }, // TODO: bug: "something is wrong with the timeline scale"
-    Tree: { type: "inline" },
-    TreeMap: { type: "inline" },
+    ImageGrid: { type: "iframe", resultBrowser: "Image" },
+    LineChart: { type: "iframe" },
+    Map: { type: "iframe", resultBrowser: "Coordinate" },
+    ScatterChart: { type: "iframe" },
+    Table: { type: "iframe" },
+    Timeline: { type: "iframe" },
+    Tree: { type: "iframe" },
+    TreeMap: { type: "iframe" },
   };
 
   //
   // Code for executing and rendering sparql
   //
   function runSparql(q) {
-    let lang = (navigator.languages && navigator.languages[0]) ||
+    let lang =
+      (navigator.languages && navigator.languages[0]) ||
       navigator.language ||
       navigator.userLanguage;
     lang = typeof lang === "string" ? lang.split("-").shift() : undefined;
     let sparqlApi = new wikibase.queryService.api.Sparql(
       "https://query.wikidata.org/sparql",
-      lang,
+      lang
     );
     return new Promise((resolve, reject) =>
-      sparqlApi.query(q).done(() => {
-        resolve(sparqlApi);
-      }).fail(() => {
-        reject(sparqlApi);
-      })
+      sparqlApi
+        .query(q)
+        .done(() => {
+          resolve(sparqlApi);
+        })
+        .fail(() => {
+          reject(sparqlApi);
+        })
     );
   }
 
@@ -45,21 +49,26 @@
     query = query.trim();
 
     let match = query.match(/#defaultView:[^a-zA-Z]*([a-zA-Z]*)(.*)/);
-    let view = visualisations[match && match[1] || "Table"];
+    let view = visualisations[(match && match[1]) || "Table"];
     try {
       if (!view) {
         throw new Error("Invalid view: " + match[0]);
       }
       if (view.type === "inline") {
         elem = replaceElem(elem, inheritElem(elem, "div"));
-        elem.innerText = "executing query";
+        elem.innerHTML = "Executing query:";
+        let sparqlElem = document.createElement("pre");
+        sparqlElem.innerText = query;
+        sparqlElem.style.fontSize = "70%";
+        sparqlElem.style.margin = "2ex";
+        elem.appendChild(sparqlElem);
 
         let sparqlApi = await runSparql(query);
         let resultBrowser = new view.resultBrowser();
         elem.innerHTML = "";
         resultBrowser.setSparqlApi(sparqlApi);
         var options = new wikibase.queryService.ui.resultBrowser.helper.Options(
-          {},
+          {}
         );
         resultBrowser.setOptions(options);
         resultBrowser.setResult(sparqlApi.getResultRawData());
@@ -72,28 +81,30 @@
         }
       } else if (view.type === "iframe") {
         let iframe = replaceElem(elem, inheritElem(elem, "iframe"));
-        iframe.src = "https://query.wikidata.org/embed.html#" +
-          encodeURIComponent(query);
+        iframe.src =
+          "https://query.wikidata.org/embed.html#" + encodeURIComponent(query);
       }
     } catch (e) {
-      elem.innerText = "Error running sparql query:" + String(e);
+      elem.innerHTML = "<h1>Error executing SPARQL-query</h1>";
+      console.log(e);
       throw e;
     }
   }
 
-
-async function sparqlToIframe(sparql, element, filename) {
+  async function sparqlToIframe(sparql, element, filename) {
     await dependencies;
-let elem = document.querySelector(element);
-      sparqlElem({ elem, query: sparql});
-/*
-    $(element).attr('src', url);
-    $(element).parent().after(
-        '<span style="float:right; font-size:smaller"><a href="https://github.com/fnielsen/scholia/blob/master/scholia/app/templates/' + filename + '">' +
-            filename.replace("_", ": ") +
-            '</a></span>');
-*/
-};
+    let elem = document.querySelector(element);
+    elem.style.clear = "both";
+    let infoLink = document.createElement("div");
+    infoLink.style.float = "right";
+    infoLink.style.fontSize = "smaller";
+    infoLink.innerHTML = `<a href="https://github.com/fnielsen/scholia/blob/master/scholia/app/templates/${filename}">${filename.replace(
+      "_",
+      ": "
+    )}</a>`;
+    elem.parentElement.after(infoLink);
+    sparqlElem({ elem, query: sparql });
+  }
 
   window.sparqlToIframe = sparqlToIframe;
   //
@@ -104,7 +115,7 @@ let elem = document.querySelector(element);
     await dependencies;
 
     const elements = document.querySelectorAll(
-      "script[type='sparql/scholia-visualised']",
+      "script[type='sparql/scholia-visualised']"
     );
     for (const elem of elements) {
       sparqlElem({ elem, query: elem.innerText });
@@ -130,74 +141,81 @@ let elem = document.querySelector(element);
 
   function loadScript(src) {
     return new Promise((resolve, reject) => {
-    let elem = document.createElement('script');
-      elem.src = '/static/' + src;
+      let elem = document.createElement("script");
+      elem.src = "/static/" + src;
       elem.onload = resolve;
       elem.onerror = reject;
-    document.head.appendChild(elem);
+      document.head.appendChild(elem);
     });
   }
 
   async function loadDependencies() {
-    await Promise.all([
-    "wikidata-query-gui/dependencies/underscore/underscore.js",
-    "wikidata-query-gui/dependencies/leaflet/dist/leaflet.js",
-    "wikidata-query-gui/dependencies/d3/d3.js",
-    "wikidata-query-gui/dependencies/moment/min/moment-with-locales.js",
-    "wikidata-query-gui/dependencies/wellknown/wellknown.js",
-    "wikidata-query-gui/dependencies/downloadjs/download.js",
-    "wikidata-query-gui/dependencies/sparqljs/dist/sparqljs-browser-min.js",
-    ].map(loadScript));
-    await Promise.all([
-    "wikidata-query-gui/wikibase/queryService/ui/i18n/getMessage.js",
-    "wikidata-query-gui/wikibase/queryService/ui/dialog/QueryExampleDialog.js",
-    "wikidata-query-gui/wikibase/queryService/ui/ResultView.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/helper/FormatterHelper.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/helper/Options.js",
-    "wikidata-query-gui/wikibase/queryService/api/Sparql.js",
-    "wikidata-query-gui/wikibase/queryService/api/Wikibase.js",
-    "wikidata-query-gui/wikibase/queryService/RdfNamespaces.js",
-    "wikidata-query-gui/wikibase/queryService/ui/queryHelper/QueryHelper.js",
-    "wikidata-query-gui/wikibase/queryService/ui/queryHelper/SparqlQuery.js",
-    "wikidata-query-gui/wikibase/queryService/ui/queryHelper/SelectorBox.js",
-    "wikidata-query-gui/wikibase/queryService/ui/queryHelper/QueryTemplate.js",
-    "wikidata-query-gui/dependencies/leaflet-fullscreen/dist/Leaflet.fullscreen.js",
-    "wikidata-query-gui/dependencies/leaflet-zoombox/L.Control.ZoomBox.js",
-    "wikidata-query-gui/dependencies/leaflet.markercluster/dist/leaflet.markercluster.js",
-    "wikidata-query-gui/dependencies/leaflet.locatecontrol/dist/L.Control.Locate.min.js",
-    "wikidata-query-gui/dependencies/leaflet-minimap/dist/Control.MiniMap.min.js",
-    "wikidata-query-gui/dependencies/dimple-js/dist/dimple.latest.js",
-    "wikidata-query-gui/dependencies/vis/dist/vis.js",
-    ].map(loadScript));
-    for(const script of [
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/AbstractResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/AbstractChartResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/AbstractDimpleChartResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/ImageResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/TableResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/CoordinateResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/TreeMapResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/TreeResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/BubbleChartResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/LineChartResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/BarChartResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/ScatterChartResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/AreaChartResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/TimelineResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/MultiDimensionResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/GraphResultBrowser.js",
-    "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/GraphResultBrowserNodeBrowser.js",
-      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/PolestarResultBrowser.js"]) 
+    await Promise.all(
+      [
+        "wikidata-query-gui/dependencies/underscore/underscore.js",
+        "wikidata-query-gui/dependencies/leaflet/dist/leaflet.js",
+        "wikidata-query-gui/dependencies/d3/d3.js",
+        "wikidata-query-gui/dependencies/moment/min/moment-with-locales.js",
+        "wikidata-query-gui/dependencies/wellknown/wellknown.js",
+        "wikidata-query-gui/dependencies/downloadjs/download.js",
+        "wikidata-query-gui/dependencies/sparqljs/dist/sparqljs-browser-min.js",
+      ].map(loadScript)
+    );
+    await Promise.all(
+      [
+        "wikidata-query-gui/wikibase/queryService/ui/i18n/getMessage.js",
+        "wikidata-query-gui/wikibase/queryService/ui/dialog/QueryExampleDialog.js",
+        "wikidata-query-gui/wikibase/queryService/ui/ResultView.js",
+        "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/helper/FormatterHelper.js",
+        "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/helper/Options.js",
+        "wikidata-query-gui/wikibase/queryService/api/Sparql.js",
+        "wikidata-query-gui/wikibase/queryService/api/Wikibase.js",
+        "wikidata-query-gui/wikibase/queryService/RdfNamespaces.js",
+        "wikidata-query-gui/wikibase/queryService/ui/queryHelper/QueryHelper.js",
+        "wikidata-query-gui/wikibase/queryService/ui/queryHelper/SparqlQuery.js",
+        "wikidata-query-gui/wikibase/queryService/ui/queryHelper/SelectorBox.js",
+        "wikidata-query-gui/wikibase/queryService/ui/queryHelper/QueryTemplate.js",
+        "wikidata-query-gui/dependencies/leaflet-fullscreen/dist/Leaflet.fullscreen.js",
+        "wikidata-query-gui/dependencies/leaflet-zoombox/L.Control.ZoomBox.js",
+        "wikidata-query-gui/dependencies/leaflet.markercluster/dist/leaflet.markercluster.js",
+        "wikidata-query-gui/dependencies/leaflet.locatecontrol/dist/L.Control.Locate.min.js",
+        "wikidata-query-gui/dependencies/leaflet-minimap/dist/Control.MiniMap.min.js",
+        "wikidata-query-gui/dependencies/dimple-js/dist/dimple.latest.js",
+        "wikidata-query-gui/dependencies/vis/dist/vis.js",
+      ].map(loadScript)
+    );
+    for (const script of [
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/AbstractResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/AbstractChartResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/AbstractDimpleChartResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/ImageResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/TableResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/CoordinateResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/TreeMapResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/TreeResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/BubbleChartResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/LineChartResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/BarChartResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/ScatterChartResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/AreaChartResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/TimelineResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/MultiDimensionResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/GraphResultBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/GraphResultBrowserNodeBrowser.js",
+      "wikidata-query-gui/wikibase/queryService/ui/resultBrowser/PolestarResultBrowser.js",
+    ])
       await loadScript(script);
 
-
     for (const name in visualisations) {
-      visualisations[name].resultBrowser = wikibase.queryService.ui
-      .resultBrowser[
-        (visualisations[name].resultBrowser || name) + "ResultBrowser"
-      ];
+      visualisations[name].resultBrowser =
+        wikibase.queryService.ui.resultBrowser[
+          (visualisations[name].resultBrowser || name) + "ResultBrowser"
+        ];
       visualisations[name].name = name;
     }
   }
 
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 })();
